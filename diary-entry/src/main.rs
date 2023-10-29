099111476e906828fa115cbe82e2ca7f07139d76
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2023-10-29 09:49:51 +0100

    Add file writing and reading functionality to main.rs
    Three key changes were made in this commit. The primary change was in src/main.rs where functions for writing to and reading from a file were added to the main function. The file written to and read from was set as "diary.txt". This necessitated the allowance of diary.txt in .gitignore file. Lastly, a new Cargo.lock file was added with package details for the new `diary-entry` functionality.

--- diary-entry/src/main.rs
+++ diary-entry/src/main.rs
@@ -1,3 +1,18 @@
+use std::fmt::Debug;
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    match fs::write("diary.txt", "this is a content") {
+        Err(e) => print!("{}", e.to_string()),
+        Ok(x) => print!("Completed !!")
+    }
+
+    let content = fs::read("diary.txt");
+
+
+    match content {
+        Ok(c) => print!("{:?}", String::from_utf8(c)),
+        Err(e) => print!("{}", e.to_string()),
+    }
 }
